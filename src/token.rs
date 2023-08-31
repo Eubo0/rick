f8af6a35870f51eb149ebe6ac1bcc22ed3100efb
Michael VZ <mikevanzyl2000@gmail.com>
2023-08-31 14:15:48 +0200

    Implemented fmt::display for tokens

--- src/token.rs
+++ src/token.rs
@@ -1,4 +1,4 @@
-
+use std::fmt;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Token {
@@ -106,6 +106,68 @@ impl Token {
             true
         } else {
             false
+        }
+    }
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), std::fmt::Error> {
+        match self {
+            Token::Identifier(s) => write!(f, "{}", s),
+            Token::StringLiteral(s) => write!(f, "{}", s),
+            Token::IntegerLiteral(n) => write!(f, "{}", n),
+            Token::FloatLiteral(n) => write!(f, "{}", n),
+
+            Token::And => write!(f, "and"),
+            Token::Or => write!(f, "or"),
+            Token::Func => write!(f, "func"),
+            Token::True => write!(f, "true"),
+            Token::False => write!(f, "false"),
+            Token::Let => write!(f, "let"),
+            Token::If => write!(f, "if"),
+            Token::Elif => write!(f, "elif"),
+            Token::Else => write!(f, "else"),
+            Token::While => write!(f, "while"),
+            Token::Return => write!(f, "return"),
+            Token::Read => write!(f, "read"),
+            Token::Print => write!(f, "print"),
+            Token::Array => write!(f, "array"),
+            Token::Integer => write!(f, "integer"),
+            Token::Float => write!(f, "float"),
+            Token::Boolean => write!(f, "boolean"),
+
+            Token::Lpar => write!(f, "("),
+            Token::Rpar => write!(f, ")"),
+            Token::Lbrace => write!(f, "{{"),
+            Token::Rbrace => write!(f, "}}"),
+            Token::Lbrack => write!(f, "["),
+            Token::Rbrack => write!(f, "]"),
+            
+            Token::Comma => write!(f, ","),
+            Token::Semicolon => write!(f, ";"),
+            Token::Colon => write!(f, ":"),
+            Token::Quote => write!(f, "\""),
+            Token::Negate => write!(f, "!"),
+
+            Token::ArrowLeft => write!(f, "<-"),
+            Token::ArrowRight => write!(f, "->"),
+            Token::Concat => write!(f, "<>"),
+
+            Token::Add => write!(f, "+"),
+            Token::Sub => write!(f, "-"),
+            Token::Mul => write!(f, "*"),
+            Token::Div => write!(f, "/"),
+            Token::Mod => write!(f, "%"),
+            Token::Pow => write!(f, "**"),
+
+            Token::Assign => write!(f, "="),
+
+            Token::Eq => write!(f, "=="),
+            Token::Neq => write!(f, "!="),
+            Token::Gt => write!(f, ">"),
+            Token::Gte => write!(f, ">="),
+            Token::Lt => write!(f, "<"),
+            Token::Lte => write!(f, "<="),
         }
     }
 }
